126c9a1acac628f6caebcee5b1e30c7f2ff910c2
rahuldhiman123 <77484447+rahuldhiman123@users.noreply.github.com>
2021-05-18 09:59:38 +0530

    Update Question_8.js

--- Question_8.js
+++ Question_8.js
@@ -1,12 +1,12 @@
-// In Question NO. 8, there is an Object array of obj. and it is wrong.  Object provided by you.
+// In Question NO. 8, there is an Object array of obj. and it might be wrong.  Object provided by you.
 // var obj = {
 //  “id” : 4, “name” : “abc”,
 //  “id” : 10, “name” : “ab2”,
 //  “id” : 5, “name” : “abc3”,
 //  “id” : 6, “name” : “abc5”
 // }
- 
-// As per my knowledge object is as 
+ // when i am process code on this json object i am getting object error. 
+// As per my knowledge object is as like.
 var obj = [
  {id : 4, name : "abc"},
  {id : 10, name : "ab2"},
